2c547f188d9a981d8807afe4b1a58e6090fc3d09
moa1128 <moa971128@gmail.com>
2023-10-18 17:09:48 +0900

    feat: set detail post

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,8 +1,12 @@
+import Head from 'next/head';
 import Layout from '../../components/layout';
+import Date from '../../components/date';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import utilStyles from '../../styles/utils.module.css';
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
+  // paths: [ { params: { id: 'pre-rendering'  } }, { params: { id: 'ssg-ssr' } } ]
   return {
     paths,
     fallback: false,
@@ -10,9 +14,8 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  // Add the "await" keyword like this:
   const postData = await getPostData(params.id);
-
+  // postData: { id: 'ssg-ssr', contentHtml: '...', title: '...', date: '2020-01-02' }
   return {
     props: {
       postData,
@@ -20,16 +23,28 @@ export async function getStaticProps({ params }) {
   };
 }
 
+// Server Side Rendering
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {
+//       // props for your component
+//     },
+//   };
+// }
+
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
